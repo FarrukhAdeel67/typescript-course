cc7e81b77783670b5c786675c46f99580a3fc49e
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-08-31 02:14:33 -0700

    Create sandbox.ts
    typescript code

---
+++ sandbox.ts
@@ -0,0 +1,7 @@
+const character = 'farrukh adeel';
+console.log(character);
+const inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(input =>{
+    console.log(input);
+})
