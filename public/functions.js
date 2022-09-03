788d736b8b847af1576671daf301010162620dce
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-09-03 12:53:12 -0700

    Update functions.js

--- public/functions.js
+++ public/functions.js
@@ -3,10 +3,12 @@ let greet;
 greet = (a, b, c = 12) => {
     console.log(a + b);
     console.log(c);
+    console.log("hello");
 };
 greet(1, 2);
-//function signatures.
 let logDetails;
 logDetails = (ninga) => {
+    ninga.name = 'farrukh';
+    ninga.age = 12;
     console.log(`${ninga.name} is ${ninga.age} years old`);
 };
