0aea7479ea8cb513ab33194eff208a537e38ff60
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-09-04 03:40:18 -0700

    extra ts for practice

---
+++ extra/ts/typescript-arrays.ts
@@ -0,0 +1,14 @@
+//typescript arrays....
+let names = ['farrukh', 'adeel', 'raziq' ];
+names.push('usman');
+console.log(names);
+let numbers = [1,3 ,5,5];
+numbers.push(2);
+console.log(numbers);
+//numbers.push('df'); error
+let mixed = ['farrukh', 2, true];
+mixed.push(3, 'adeel');
+mixed[0] = 'khan';
+console.log(mixed);
+
+
