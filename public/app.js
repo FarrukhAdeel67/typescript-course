4099b18e70cad2ad171e0ad366a90a76a96b575c
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-09-04 16:37:01 -0700

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,5 +1,6 @@
 import { Payment } from "./classes/payment.js";
 import { Invoice } from "./classes/invoice.js";
+import { listTemplate } from "./classes/listTemplate.js";
 //form
 const form = document.querySelector('.new-item-form');
 //inputs
@@ -7,14 +8,21 @@ const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+//list template instance
+const ul = document.querySelector('ul');
+const list = new listTemplate(ul);
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    let values; //tuples
+    values = [tofrom.value, details.value, amount.valueAsNumber];
     let doc;
     if (type.value === 'invoice') {
-        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+        //   doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     }
     else {
-        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
+        //  doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
+    list.render(doc, type.value, 'end');
 });
